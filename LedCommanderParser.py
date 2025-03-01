a2ffb3ba78a753da305559f5fa1638ffbf604847
Simon Vetter <git@powerswitch-entertainment.de>
2025-03-01 21:58:06 +0100

    Add serializer to write file
    Add functions to write a file from previously parsed values.

--- LedCommanderParser.py
+++ LedCommanderParser.py
@@ -77,6 +77,25 @@ class Scene:
 
         return self
 
+    def serialize_to(self, writefile: BinaryIO) -> None:
+        for fixture_id in range(LedCommanderParser.FIXTURES_COUNT):
+            for value_id in range(LedCommanderParser.CHANNELS_COUNT):
+                writefile.write(self.fixture_channel_values[fixture_id][value_id].to_bytes(1, 'little'))
+
+        index = 0
+        for byte_id in range(20):
+            value = 0
+            for bit_id in range(8):
+                fixture, channel = divmod(index, LedCommanderParser.CHANNELS_COUNT)
+                if self.fixture_channel_active[fixture][channel]:
+                    value |= (1 << bit_id)
+                index += 1
+            writefile.write(value.to_bytes(1, 'little'))
+
+        writefile.write(self.mystery_flags_1)
+        writefile.write(self.number_of_values.to_bytes(1, 'little'))
+        writefile.write(self.mystery_flags_2)
+
 
 class LedCommanderParser:
     FIXTURES_COUNT: int = 16
@@ -87,7 +106,7 @@ class LedCommanderParser:
     STATIC_SCENES_COUNT: int = 16
     CHASE_STEP_COUNT: int = 2000
 
-    def __init__(self, file):
+    def __init__(self):
         self.channel_names = [b"unknown"] * self.CHANNELS_NAMES_COUNT
         self.default_channel_names = self._create_default_channel_names()
         self.dmx_assignments: List[Optional[Tuple[Optional[int], int]]] = [None] * self.DMX_CHANNELS_COUNT
@@ -99,28 +118,47 @@ class LedCommanderParser:
         self.chase_steps: List[Scene] = [Scene()] * self.CHASE_STEP_COUNT
         self.chases: List[Chase] = [Chase()] * self.CHASES_COUNT
 
-        with open(file, "rb") as readfile:
-            self.is_magic_number_ok = self._read_and_check_magic_number(readfile)
-            if not self.is_magic_number_ok:
+    @classmethod
+    def from_file(cls, filename: str):
+        instance = cls()
+        with open(filename, "rb") as readfile:
+            instance.is_magic_number_ok = instance._read_and_check_magic_number(readfile)
+            if not instance.is_magic_number_ok:
                 error("Magic number missing! Abort")
-                return
+                return None
             info(f"Magic number ok")
 
-            self._read_scenes(readfile)
-            self._read_names(readfile)
-            self._read_dmx_channel_assignments(readfile)
-            self._read_chase_info(readfile)
-            self.is_magic_number_ok = self._read_acme_info(readfile)
-            if not self.is_magic_number_ok:
+            instance._read_scenes(readfile)
+            instance._read_names(readfile)
+            instance._read_dmx_channel_assignments(readfile)
+            instance._read_chase_info(readfile)
+            instance.is_magic_number_ok = instance._read_acme_info(readfile)
+            if not instance.is_magic_number_ok:
                 error("ACME string missing! Abort")
-                return
+                return None
 
-            self._read_mystery_dmx_info(readfile)
-            self._read_chase_step_assignments(readfile)
-            self._read_random_bytes(readfile)
-            self._read_virtual_dimmer_modes(readfile)
-            self._read_virtual_dimmer_assignments(readfile)
-            self._read_rest(readfile)
+            instance._read_mystery_dmx_info(readfile)
+            instance._read_chase_step_assignments(readfile)
+            instance._read_random_bytes(readfile)
+            instance._read_virtual_dimmer_modes(readfile)
+            instance._read_virtual_dimmer_assignments(readfile)
+            instance._read_rest(readfile)
+        return instance
+
+    def to_file(self, filename: str):
+        with open(filename, "wb") as writefile:
+            self._write_magic_number(writefile)
+            self._write_scenes(writefile)
+            self._write_names(writefile)
+            self._write_dmx_channel_assignments(writefile)
+            self._write_chase_info(writefile)
+            self._write_acme_info(writefile)
+            self._write_mystery_dmx_info(writefile)
+            self._write_chase_step_assignments(writefile)
+            self._write_random_bytes(writefile)
+            self._write_virtual_dimmer_modes(writefile)
+            self._write_virtual_dimmer_assignments(writefile)
+            self._write_rest(writefile)
 
     @staticmethod
     def _create_default_channel_names() -> List[str]:
@@ -160,6 +198,18 @@ class LedCommanderParser:
         magic_number = readfile.read(512)
         return magic_number == b"succeeded" + (b"\x00" * 503)
 
+    @staticmethod
+    def _write_magic_number(writefile: BinaryIO) -> None:
+        """
+        Write first 512 bytes of file.
+
+        Contains "succeeded" and \x00 for rest of 512 byte block.
+        Writes file offset 0x00000 - 0x00199
+
+        :param readfile: file to write bytes into.
+        """
+        writefile.write(b"succeeded" + (b"\x00" * 503))
+
     def _read_scenes(self, readfile: BinaryIO) -> None:
         """
         Read blocks that contain information about scenes and chase steps.
@@ -181,6 +231,20 @@ class LedCommanderParser:
                 info(f"Chase step {block_id + 1}:")
                 scene.print()
             self.chase_steps[block_id] = scene
+
+    def _write_scenes(self, writefile: BinaryIO) -> None:
+        """
+        Write blocks that contain information about scenes and chase steps.
+
+        Writes file offset 0x00200 - 0x5AAFF
+
+        :param writefile: file to write bytes into.
+        """
+        for static_scene_id in range(self.STATIC_SCENES_COUNT):
+            self.static_scenes[static_scene_id].serialize_to(writefile)
+
+        for block_id in range(self.CHASE_STEP_COUNT):
+            self.chase_steps[block_id].serialize_to(writefile)
 
     @staticmethod
     def _read_name(readfile: BinaryIO) -> bytes:
@@ -216,12 +280,23 @@ class LedCommanderParser:
             self.channel_names[channel_id] = channel_name
             info(f"{self.default_channel_names[channel_id]} name: '{self._name_to_str(channel_name)}'")
 
+    def _write_names(self, writefile: BinaryIO) -> None:
+        """
+        Write names of channels, pan, tilt and aux buttons.
+
+        Writes file offset 0x5AB00 - 0x5AB53
+
+        :param writefile: file to write bytes into.
+        """
+        for channel_id in range(self.CHANNELS_NAMES_COUNT):
+            writefile.write(self.channel_names[channel_id])
+
     def _read_dmx_channel_assignments(self, readfile: BinaryIO) -> None:
         """
         Read assignments of DMX channels to Fixtures+Channel or Aux 1/Aux 2.
 
         Parses file offset 0x5AB54 - 0x5AD53
-
+<
         :param readfile: file to read bytes from.
         """
         for dmx_channel in range(self.DMX_CHANNELS_COUNT):
@@ -240,6 +315,27 @@ class LedCommanderParser:
                 info(f"DMX Channel {dmx_channel + 1}: "
                      f"Fixture {dmx_fixture + 1}: "
                      f"{self._get_channel_name(dmx_channel_of_fixture)}")
+
+    def _write_dmx_channel_assignments(self, writefile: BinaryIO) -> None:
+        """
+        Write assignments of DMX channels to Fixtures+Channel or Aux 1/Aux 2.
+
+        Writes file offset 0x5AB54 - 0x5AD53
+
+        :param writefile: file to write bytes into.
+        """
+        for dmx_channel in range(self.DMX_CHANNELS_COUNT):
+            dmx_channel_assignment = self.dmx_assignments[dmx_channel]
+            if dmx_channel_assignment is None:
+                writefile.write(b'\xA2')
+            elif dmx_channel_assignment[1] == 11:
+                writefile.write(b'\xA1')
+            elif dmx_channel_assignment[1] == 10:
+                writefile.write(b'\xA0')
+            else:
+                writefile.write(
+                    (dmx_channel_assignment[0] * self.CHANNELS_COUNT + dmx_channel_assignment[1]).to_bytes(1, 'little')
+                )
 
     def _read_chase_info(self, readfile: BinaryIO) -> None:
         """
@@ -261,6 +357,18 @@ class LedCommanderParser:
             chase.step_count = number_of_steps_in_chase
             self.chases[chase_id] = chase
 
+    def _write_chase_info(self, writefile: BinaryIO) -> None:
+        """
+        Write information about chases that aren't clear yet. Appears to be {n}\x05
+
+        Write file offset 0x5AD54 - 0x5AD83
+
+        :param writefile: file to write bytes into.
+        """
+        for chase_id in range(self.CHASES_COUNT):
+            writefile.write(self.chases[chase_id].step_count.to_bytes(2, 'little'))
+            writefile.write(b"\x05")  # unknown
+
     @staticmethod
     def _read_acme_info(readfile: BinaryIO) -> bool:
         """
@@ -272,6 +380,17 @@ class LedCommanderParser:
         """
         return readfile.read(5) == b"acme\x00"
 
+    @staticmethod
+    def _write_acme_info(writefile: BinaryIO) -> None:
+        """
+        Write acme string
+
+        Writes file offset 0x5AD784 - 0x5AD88
+
+        :param writefile: file to write bytes into.
+        """
+        writefile.write(b"acme\x00")
+
     def _read_mystery_dmx_info(self, readfile: BinaryIO) -> None:
         """
         Read information about DMX channels, perhaps. Appears to be always \x02
@@ -282,6 +401,17 @@ class LedCommanderParser:
         """
         for fixture_id in range(self.DMX_CHANNELS_COUNT):
             readfile.read(1)
+
+    def _write_mystery_dmx_info(self, writefile: BinaryIO) -> None:
+        """
+        Write information about DMX channels, perhaps. Appears to be always \x02
+
+        Writes file offset 0x5AD89 - 0x5AF88
+
+        :param writefile: file to write bytes into.
+        """
+        for fixture_id in range(self.DMX_CHANNELS_COUNT):
+            writefile.write(b"\x02")  # unknown
 
     def _read_chase_step_assignments(self, readfile: BinaryIO) -> None:
         """
@@ -300,6 +430,18 @@ class LedCommanderParser:
                 info(f"Chase {chase_id + 1}")
                 self.chases[chase_id].print()
 
+    def _write_chase_step_assignments(self, writefile: BinaryIO) -> None:
+        """
+        Write assignment of chase steps to chase scenes
+
+        Writes file offset 0x5AF89 - 0x6A988
+
+        :param writefile: file to write bytes into.
+        """
+        for chase_id in range(self.CHASES_COUNT):
+            for step in range(self.CHASE_STEP_COUNT):
+                writefile.write(self.chases[chase_id].step_ids[step].to_bytes(2, 'little'))
+
     @staticmethod
     def _read_random_bytes(readfile: BinaryIO) -> None:
         """
@@ -310,6 +452,17 @@ class LedCommanderParser:
         :param readfile: file to read bytes from.
         """
         readfile.read(3)
+
+    @staticmethod
+    def _write_random_bytes(writefile: BinaryIO) -> None:
+        """
+        Write bytes that couldn't be decoded yet (0x4B 0x01 0x00)
+
+        Writes file offset 0x6A989 - 0x6A98C
+
+        :param writefile: file to write bytes into.
+        """
+        writefile.write(b"\x4B\x01\x00")
 
     @staticmethod
     def _read_virtual_dimmer_mode(readfile: BinaryIO) -> int:
@@ -333,6 +486,17 @@ class LedCommanderParser:
         for fixture_id in range(self.FIXTURES_COUNT):
             self.virtual_dimmer_modes[fixture_id] = self._read_virtual_dimmer_mode(readfile)
 
+    def _write_virtual_dimmer_modes(self, writefile: BinaryIO) -> None:
+        """
+        Write mode of virtual dimmer, off, RGB or RGBAUVW (Depending on FW>=1.5).
+
+        Writes file offset 0x6A98D - 0x6A99C
+
+        :param writefile: file to write bytes into.
+        """
+        for fixture_id in range(self.FIXTURES_COUNT):
+            writefile.write(self.virtual_dimmer_modes[fixture_id].to_bytes(1, 'little'))
+
     @staticmethod
     def _read_virtual_dimmer_assignment(readfile: BinaryIO) -> int:
         """
@@ -354,6 +518,18 @@ class LedCommanderParser:
             for channel_id in range(self.CHANNELS_COUNT):
                 self.virtual_dimmer_assignments[fixture_id][channel_id] = self._read_virtual_dimmer_assignment(readfile)
 
+    def _write_virtual_dimmer_assignments(self, writefile: BinaryIO) -> None:
+        """
+        Write if virtual dimmer should be applied onto channel of fixture.
+
+        Writes file offset 0x6A99D - 0x6AA3C
+
+        :param writefile: file to write bytes into.
+        """
+        for fixture_id in range(self.FIXTURES_COUNT):
+            for channel_id in range(self.CHANNELS_COUNT):
+                writefile.write(self.virtual_dimmer_assignments[fixture_id][channel_id].to_bytes(1, 'little'))
+
     @staticmethod
     def _read_rest(readfile: BinaryIO) -> bool:
         """
@@ -366,10 +542,23 @@ class LedCommanderParser:
         rest = readfile.read()
         return rest == b"\xFF" * len(rest)
 
+    @staticmethod
+    def _write_rest(writefile: BinaryIO) -> None:
+        """
+        Write rest of file, which should consist only of 0xFF.
+
+        Writes file offset 0x6AA3D - 0x801FF
+
+        :param writefile: file to write bytes into.
+        """
+        writefile.write(b"\x00")
+        writefile.write(b"\xFF" * 88003)
+
 
 if __name__ == "__main__":
     import sys
 
     logging.basicConfig(level=logging.DEBUG)
 
-    lcp = LedCommanderParser(sys.argv[1])
+    lcp = LedCommanderParser.from_file(sys.argv[1])
+    # lcp.to_file(sys.argv[1] + ".new")
